85cb7349944f1c1f11c6c819889c80df81c30425
Michał Wieczorek <mw301883@student.polsl.pl>
2023-08-21 15:33:15 +0200

    Working on admin login option

---
+++ restaurantservice/src/main/java/pl/michalwieczorek/restaurantservice/Configuration/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package pl.michalwieczorek.restaurantservice.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+}
